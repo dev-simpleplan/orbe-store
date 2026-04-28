56ca7b22df262f05b6f8abab7e788b845376c0d3
dev-simpleplan <95477430+dev-simpleplan@users.noreply.github.com>
2026-04-28 18:56:34 +0530

    update

--- assets/custom.js
+++ assets/custom.js
@@ -6,3 +6,29 @@ document.addEventListener("DOMContentLoaded", () => {
         mobileNav.classList.toggle("active");
     });
 });
+
+$(window).on('load', function () {
+    var owl = $('.blogs-feature-card-wrap');
+
+    owl.owlCarousel({
+        loop: true,
+        margin: 10,
+        items: 3,
+        dots: false,
+        nav: false, // disable default nav
+        responsive: {
+            0: { items: 1 },
+            600: { items: 2 },
+            1000: { items: 3, loop: false }
+        }
+    });
+
+    // Custom navigation
+    $('.next-btn').click(function () {
+        owl.trigger('next.owl.carousel');
+    });
+
+    $('.prev-btn').click(function () {
+        owl.trigger('prev.owl.carousel');
+    });
+});
