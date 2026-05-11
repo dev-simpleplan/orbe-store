03039bee25a914f1f65b06c0d699c5f5ae46c3c6
dev-simpleplan <95477430+dev-simpleplan@users.noreply.github.com>
2026-05-11 18:10:43 +0530

    update

--- assets/custom.js
+++ assets/custom.js
@@ -31,11 +31,12 @@ $(window).on('load', function () {
         loop: false,
         items: 3,
         dots: false,
+        margin: 16,
         nav: false,
         responsive: {
-            0: { items: 1 },
-            600: { items: 2 },
-            1000: { items: 3 }
+            0: { items: 2, margin: 16 },
+            600: { items: 2, margin: 16 },
+            1000: { items: 3, margin: 35 }
         }
     });
 
